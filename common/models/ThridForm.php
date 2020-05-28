34ccb2bc238cbdf6e08e6a9af24fbeff04b3afef
王瑀轩 <xuan96124@qq.com>
2020-05-28 18:30:47 +0800

    添加openid登录模型类

---
+++ common/models/ThridForm.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace common\models;
+
+
+use yii\base\Model;
+
+/**
+ * 第三方模型
+ *
+ * Class ThridLoginForm
+ * @package common\models
+ */
+class ThridForm extends Model
+{
+    const SCENARIO_LOGIN = 'login';
+    const SCENARIO_REGISTER = 'register';
+    public $openid;
+    private $_user;
+
+    public function rules()
+    {
+        return [
+            //openid必填
+            ['openid','required','on'=>[self::SCENARIO_LOGIN,self::SCENARIO_REGISTER]],
+            ['openid','validateOpenid','on'=>[self::SCENARIO_LOGIN]],
+            ['openid','unique','on'=>[self::SCENARIO_REGISTER]],
+            [['avatar','client','username','gender'],'required','on'=>[self::SCENARIO_REGISTER]],
+        ];
+    }
+
+    //场景：登陆和注册
+    public function scenarios()
+    {
+        return[
+            self::SCENARIO_LOGIN => ['openid'],
+            self::SCENARIO_REGISTER => ['openid','avatar','client','username','gender']
+        ];
+    }
+
+    /**
+     * 验证openId
+     *
+     * @param $attribute
+     * @param $params
+     */
+    public function validateOpenid($attribute,$params)
+    {
+        if(!$this->hasErrors()){
+            $user = $this->getUser();
+            if(!$user || !$user->role == 1){
+                $this->addError($attribute,'您不是管理员，无权登陆！');
+            }
+        }
+    }
+
+    /**
+     * 获取用户信息
+     *
+     * @return User|null
+     */
+    protected function getUser()
+    {
+        if($this->_user === null){
+            $this->_user = User::findByOpenId($this->openid);
+        }
+
+        return $this->_user;
+    }
+
+}
