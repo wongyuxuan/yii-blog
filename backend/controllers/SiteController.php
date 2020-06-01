74580d60b8255fd4e5c67446819cda62ff3fe5ab
unknown <xuan96124@qq.com>
2020-06-01 22:05:02 +0800

    修改

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -4,10 +4,29 @@
 namespace backend\controllers;
 
 
+use yii\filters\AccessControl;
 use yii\web\Controller;
 
 class SiteController extends Controller
 {
+    public function behaviors()
+    {
+       return [
+           'access' => [
+               'class' => AccessControl::className(),
+               'only' => ['index','welcome'],
+               'rules' => [
+                   // 允许认证用户
+                   [
+                       'allow' => false,
+                       'actions' => ['index'],
+                       'roles' => ['@'],
+                   ],
+               ]
+           ]
+       ];
+    }
+
     public function actions()
     {
        return [
