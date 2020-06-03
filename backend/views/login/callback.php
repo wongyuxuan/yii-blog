75e7c034e69532070cef5c015421de63989011c2
unknown <xuan96124@qq.com>
2020-06-03 22:59:26 +0800

    修改

--- backend/views/login/callback.php
+++ backend/views/login/callback.php
@@ -5,12 +5,10 @@ use yii\helpers\Url;
 
 <?php $this->beginBlock('script'); ?>
 <script>
-    $(function () {
-        layui.use('layer', function(){ //独立版的layer无需执行这一句
-            var $ = layui.jquery, layer = layui.layer; //独立版的layer无需执行这一句
-            window.parent.location.reload();
-            layer.closeAll();
-        });
-    })
+    layui.use('layer', function(){ //独立版的layer无需执行这一句
+        var $ = layui.jquery, layer = layui.layer; //独立版的layer无需执行这一句
+        window.parent.location.reload();
+        layer.closeAll();
+    });
 </script>
 <?php $this->endBlock(); ?>
