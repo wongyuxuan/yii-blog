105662da6c39ae667b600c0b01e44b558a9fa955
王瑀轩 <xuan96124@qq.com>
2020-06-03 15:48:24 +0800

    修改

--- backend/views/login/callback.php
+++ backend/views/login/callback.php
@@ -3,14 +3,6 @@ $this->title = 'qq登录';
 use yii\helpers\Url;
 ?>
 
-<?php $this->beginBlock('style'); ?>
-<style>
-    body{
-        background: url("<?= Url::to('@img/loginBg.jpg') ?>") no-repeat;
-        background-size: cover;
-    }
-</style>
-<?php $this->endBlock(); ?>
 <?php $this->beginBlock('script'); ?>
 <script>
     layui.use('layer', function(){ //独立版的layer无需执行这一句
